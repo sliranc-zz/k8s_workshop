5d7af64ef9a38e9ca67e876570b36749a52ff6f5
root <root@ctor-kna001.tlv.lpnet.com>
2016-05-29 22:34:42 +0300

    new rc

--- cli_demo/filepuller/filepuller.sh
+++ cli_demo/filepuller/filepuller.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+touch /usr/share/nginx/html/pod.html
+echo $HOSTNAME > /usr/share/nginx/html/pod.html
 while true; do 
 curl -o /usr/share/nginx/html/index.html https://raw.githubusercontent.com/sliranc/k8s_workshop/master/cli_demo/filepuller/data/index.html
 sleep 10
