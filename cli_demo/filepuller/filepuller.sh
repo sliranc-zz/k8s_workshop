bcb33cdd3c8cb1fb4b0a4b4fb9c7b0839c6299ca
root <root@ip-10-0-0-197.eu-west-1.compute.internal>
2016-05-30 18:14:11 +0000

    move docker

--- cli_demo/filepuller/filepuller.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-touch /usr/share/nginx/html/pod.html
-echo $HOSTNAME > /usr/share/nginx/html/pod.html
-while true; do 
-curl -o /usr/share/nginx/html/index.html https://raw.githubusercontent.com/sliranc/k8s_workshop/master/cli_demo/filepuller/data/index.html
-sleep 10
-
-done
--- cli_demo/logme/logme.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-while true; do 
-echo "$(date) running inside kubernetes pod ${HOSTNAME}"
-sleep 1
-
-done
