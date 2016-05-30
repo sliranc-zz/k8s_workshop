f50bb6c17cbdeb87fea2f19defa2647941de723f
root <root@ctor-kna001.tlv.lpnet.com>
2016-05-30 12:36:26 +0300

    new rcweb yaml

---
+++ cli_demo/docker/filepuller/filepuller.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+touch /usr/share/nginx/html/pod.html
+echo $HOSTNAME > /usr/share/nginx/html/pod.html
+git clone https://github.com/sliranc/k8s_workshop.git
+cd k8s_workshop
+while true; do 
+git pull
+cp -apv cli_demo/filepuller/data/* /usr/share/nginx/html
+sleep 30
+
+done
